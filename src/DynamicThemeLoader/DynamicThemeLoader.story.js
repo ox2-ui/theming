417a8bac7bd83f8b3694c63accbf7d597d3ceff3
Lauricio Su <me@lauricio.com>
2017-04-05 17:12:12 +0700

    ❌ [DynamicThemeLoader] remove component

--- src/DynamicThemeLoader/DynamicThemeLoader.story.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@kadira/storybook';
-import DynamicThemeLoader from './DynamicThemeLoader';
-import { default as theme } from '@tapfuse/theme-spark';
-
-storiesOf(
-  'DynamicThemeLoader',
-  module,
-).add('loader', () => <DynamicThemeLoader theme={theme} />);
